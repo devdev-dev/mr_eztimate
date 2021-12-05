7b624206d63b357ae64166615cee6ee23b6518f7
Hendrik Schuster <h.schuster@deviantdev.com>
2021-12-05 21:52:00 +0100

    Restructure the home repo

---
+++ home/src/pages/index.tsx
@@ -0,0 +1,14 @@
+import Link from "next/link";
+import React from "react";
+
+const IndexPage = () => {
+  return (
+    <div>
+      <Link href="/eztimate">
+        <a>Eztimate</a>
+      </Link>
+    </div>
+  );
+};
+
+export default IndexPage;
