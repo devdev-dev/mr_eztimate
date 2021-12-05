aa4583c126d0c8395e16d39d79d2c8d6cb791ee7
Hendrik Schuster <h.schuster@deviantdev.com>
2021-12-05 13:15:20 +0100

    Introduce an reusable AppLayout to use the Chakra UI app shell

--- eztimate-quick/src/pages/_app.tsx
+++ eztimate-quick/src/pages/_app.tsx
@@ -1,8 +1,19 @@
 import '../styles/globals.css'
 import {ApolloClient, ApolloProvider, InMemoryCache} from "@apollo/client";
 import { ChakraProvider } from '@chakra-ui/react'
+import {NextPage} from "next";
 import type { AppProps } from 'next/app'
+import dynamic from "next/dynamic";
+import {ReactElement, ReactNode, useEffect} from "react";
 import theme from "../styles/theme";
+
+type NextPageWithLayout = NextPage & {
+    getLayout?: (page: ReactElement) => ReactNode;
+};
+
+type AppPropsWithLayout = AppProps & {
+    Component: NextPageWithLayout;
+};
 
 export const apolloClient = new ApolloClient({
   uri: '/eztimate/api/graphql',
@@ -10,14 +21,37 @@ export const apolloClient = new ApolloClient({
   connectToDevTools: true
 });
 
-function MyApp({ Component, pageProps }: AppProps) {
+function MyApp({ Component, pageProps }: AppPropsWithLayout) {
+  useServiceWorker();
+
+  const getLayout = Component.getLayout ?? (page => page);
+
   return (
       <ChakraProvider theme={theme} resetCSS>
         <ApolloProvider client={apolloClient}>
-          <Component {...pageProps} />
+            {getLayout(<Component {...pageProps} />)}
         </ApolloProvider>
       </ChakraProvider>
   )
 }
 
-export default MyApp
+function useServiceWorker() {
+    useEffect(() => {
+        if ('serviceWorker' in navigator) {
+            window.addEventListener('load', function () {
+                navigator.serviceWorker.register('/serviceworker.js').then(
+                    function (registration) {
+                        console.log('Service Worker registration successful with scope: ', registration.scope);
+                    },
+                    function (err) {
+                        console.log('Service Worker registration failed: ', err);
+                    }
+                );
+            });
+        }
+    }, []);
+}
+
+export default dynamic(() => Promise.resolve(MyApp), {
+    ssr: true
+});
