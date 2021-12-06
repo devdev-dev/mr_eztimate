af4ffd43e05f31c90505c2600547c7f54365be48
Hendrik Schuster <h.schuster@deviantdev.com>
2021-12-06 22:24:59 +0100

    Remove the user section as it is currently not necessary

--- home/src/components/layout/AppLayout.tsx
+++ home/src/components/layout/AppLayout.tsx
@@ -1,22 +1,7 @@
-import {
-  Avatar,
-  Box,
-  Flex,
-  HStack,
-  IconButton,
-  Menu,
-  MenuButton,
-  MenuDivider,
-  MenuItem,
-  MenuList,
-  Text,
-  useColorMode,
-  useColorModeValue,
-  VStack
-} from '@chakra-ui/react';
+import { Box, Flex, HStack, IconButton, Text, useColorMode, useColorModeValue } from '@chakra-ui/react';
 import * as React from 'react';
 import { IconType } from 'react-icons';
-import { FiBell, FiChevronDown, FiCompass, FiHome, FiMoon, FiSettings, FiStar, FiSun, FiTrendingUp } from 'react-icons/fi';
+import { FiCompass, FiHome, FiMoon, FiSettings, FiStar, FiSun, FiTrendingUp } from 'react-icons/fi';
 
 export interface AppLayoutProps {
   children: React.ReactElement;
@@ -61,38 +46,7 @@ const MobileNav = () => {
       </Text>
 
       <HStack spacing={{ base: '0', md: '6' }}>
-        <IconButton size="lg" variant="ghost" aria-label="open menu" icon={<FiBell />} />
         <IconButton size="lg" variant="ghost" aria-label="open menu" icon={colorMode === 'light' ? <FiMoon /> : <FiSun />} onClick={toggleColorMode} />
-        <Flex alignItems={'center'}>
-          <Menu>
-            <MenuButton py={2} transition="all 0.3s" _focus={{ boxShadow: 'none' }}>
-              <HStack>
-                <Avatar
-                  size={'sm'}
-                  src={
-                    'https://images.unsplash.com/photo-1619946794135-5bc917a27793?ixlib=rb-0.3.5&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&s=b616b2c5b373a80ffc9636ba24f7a4a9'
-                  }
-                />
-                <VStack display={{ base: 'none', md: 'flex' }} alignItems="flex-start" spacing="1px" ml="2">
-                  <Text fontSize="sm">Justina Clark</Text>
-                  <Text fontSize="xs" color="gray.600">
-                    Admin
-                  </Text>
-                </VStack>
-                <Box display={{ base: 'none', md: 'flex' }}>
-                  <FiChevronDown />
-                </Box>
-              </HStack>
-            </MenuButton>
-            <MenuList bg={useColorModeValue('white', 'gray.900')} borderColor={useColorModeValue('gray.200', 'gray.700')}>
-              <MenuItem>Profile</MenuItem>
-              <MenuItem>Settings</MenuItem>
-              <MenuItem>Billing</MenuItem>
-              <MenuDivider />
-              <MenuItem>Sign out</MenuItem>
-            </MenuList>
-          </Menu>
-        </Flex>
       </HStack>
     </Flex>
   );
